c3770f9735cc2782218b410e4b6950dc969704ed
Pietro Rupil <rupilpietro@gmail.com>
2026-03-15 00:22:24 +0100

    aggiunto salto

--- gameview.py
+++ gameview.py
@@ -1,21 +1,34 @@
 import arcade
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
 class GameView(arcade.View):
     def __init__(self):
         super().__init__()
         
         arcade.set_background_color(arcade.color.DARK_GREEN)
 
+        #player
         self.lista_player = arcade.SpriteList()
         self.player = None
         self.speed: int = 5
         self.scale = 0.11
+        self.player_jump_speed = 20
+
+        #pavimento
+        self.lista_pavimento = arcade.SpriteList()
+
+        #gravita
+        self.physics_engine = 0
 
         # movimento
         self.left_pressed = False
         self.right_pressed = False
 
         self.setup()
+        self.pavimento()
+        self.CreGravita()
     
     def setup(self): # player
 
@@ -25,8 +38,26 @@ class GameView(arcade.View):
         self.player.scale = self.scale
         self.lista_player.append(self.player)
     
+    def pavimento(self):
+        self.floor = arcade.SpriteSolidColor(SCREEN_WIDTH, 20, arcade.color.GREEN)
+        self.floor.center_x = SCREEN_WIDTH / 2
+        self.floor.center_y = 10 #posizionamento sul fondo
+        self.floor.visible = False
+
+        self.lista_pavimento.append(self.floor)
+    
+    def CreGravita(self):
+        self.gravita = 1.0
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player,
+            gravity_constant = self.gravita,
+            walls = self.lista_pavimento
+        )
+
     def on_draw(self):
         self.clear()
+
+        self.lista_pavimento.draw()
         self.lista_player.draw()
     
     def on_update(self, delta_time):
@@ -48,12 +79,17 @@ class GameView(arcade.View):
         elif change_x > 0:
             self.player.scale = (self.scale, self.scale)
 
+        self.physics_engine.update()
+
     def on_key_press(self, tasto, modificatori):
 
         if tasto in (arcade.key.LEFT, arcade.key.A):
             self.left_pressed = True
         elif tasto in (arcade.key.RIGHT, arcade.key.D):
-            self.right_pressed = True          
+            self.right_pressed = True        
+        elif tasto == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player.change_y = self.player_jump_speed
 
     def on_key_release(self, tasto, modificatori):
 
